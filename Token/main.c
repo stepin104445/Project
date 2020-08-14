c5af64dfd12012b0c6c37be0c592de62120eec20
stepin104445 <69110250+stepin104445@users.noreply.github.com>
2020-08-14 09:03:47 +0530

    Update main.c

--- Token/main.c
+++ Token/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <stdlib.h>
+
+
 
 int main() {
     char *h;
     char s[]={"my_name_is_edcast_future_skills"};
 
 
-    //Write your logic to print the tokens of the sentence here.
+   
     char s1[2]="_";
     h= strtok(s,s1);
     while(h!=NULL)
